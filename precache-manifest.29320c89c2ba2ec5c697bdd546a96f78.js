a85046f1c1032c28af296f7955841957ef6bcebb
lizihangRK <a77877@dingtali.com>
2021-06-04 14:16:24 +0800

    first

---
+++ precache-manifest.29320c89c2ba2ec5c697bdd546a96f78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288b672c1774d1c4c2c7dc204438994a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf3d9a9d9d22103ecb55",
+    "url": "./static/css/2.8d0fb6d7.chunk.css"
+  },
+  {
+    "revision": "a98f8b17746794359adc",
+    "url": "./static/css/main.8e449b32.chunk.css"
+  },
+  {
+    "revision": "cf3d9a9d9d22103ecb55",
+    "url": "./static/js/2.a00a9808.chunk.js"
+  },
+  {
+    "revision": "a98f8b17746794359adc",
+    "url": "./static/js/main.7b707e35.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c3f431664b05cd5c338abe0a53bb400d",
+    "url": "./static/media/logo.c3f43166.svg"
+  },
+  {
+    "revision": "a76e4a8196c588206b2e07d20035d3df",
+    "url": "./static/media/logo1.a76e4a81.svg"
+  }
+]);
